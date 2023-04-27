a544cb9258ab49d49e23a5a1d6ce9d7dd8700666
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-04-27 21:42:04 +0300

    Arithmetic Lab Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+
+let num1 = 8,
+  num2 = 8;
+let multiply = num1 * num2 - 2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 20,
+  num4 = 16;
+let mod = num3 % num4;
+let max = Math.max(5, 6, 7, 8, 20); //20
